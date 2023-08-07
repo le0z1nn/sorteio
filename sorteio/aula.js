517cbeaba04e73a11cdcf015ec523a6f5ebfd58c
ozin <133661986+le0z1nn@users.noreply.github.com>
2023-08-07 14:46:15 -0300

    Add files via upload

---
+++ sorteio/aula.js
@@ -0,0 +1,18 @@
+// math.random gera um numero aleatorio
+console.log(Math.random());
+
+// gerando um numero aleatorio entre 0 e 20
+console.log(Math.random() * 20);
+console.log(Math.round(Math.random() * 5))
+
+// math.round arredonda para o inteiro mais proximo 
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+// math.ceil arredonda para cima
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(3.9));
+
+// Math.floor arredonda para baixo
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
